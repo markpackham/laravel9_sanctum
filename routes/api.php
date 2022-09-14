66c6c79e8b47358b398328ced2fbf893c99018fc
mark <markpackham1@gmail.com>
2022-09-14 11:50:36 +0100

    add Task resource route
    https://youtu.be/TzAJfjCn7Ks?t=1496

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::resource('/tasks', TaskController::class);
