f41d5fd8607bb94e3d3597e098dfe701ad17f01d
mark <markpackham1@gmail.com>
2022-09-14 15:03:08 +0100

    finish off login
    https://youtu.be/TzAJfjCn7Ks?t=2428

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
@@ -16,6 +17,17 @@ class AuthController extends Controller
     public function login(LoginUserRequest $request)
     {
         $request->validated($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Creds do not match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken
+        ]);
     }
 
     public function register(StoreUserRequest $request)
