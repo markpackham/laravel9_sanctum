ea84f3918cbe036577c1f41b4eaa09c02c7206c9
mark <markpackham1@gmail.com>
2022-09-14 12:46:25 +0100

    setup user register request
    https://youtu.be/TzAJfjCn7Ks?t=1759

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\HttpResponses;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreUserRequest;
 
 class AuthController extends Controller
 {
@@ -14,8 +17,15 @@ class AuthController extends Controller
         return 'This is my login Method';
     }
 
-    public function register()
+    public function register(StoreUserRequest $request)
     {
+        $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
         return response()->json('This is my register method');
     }
 
