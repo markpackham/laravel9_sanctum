5b1fe8bcc9db43dc46f32bc0d635d145d60aee35
mark <markpackham1@gmail.com>
2022-09-14 15:10:49 +0100

    test authentication
    https://youtu.be/TzAJfjCn7Ks?t=2648

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// Public routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::resource('/tasks', TaskController::class);
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('/tasks', TaskController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
